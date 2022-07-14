0d4141c6b804ddec837ad992284be6108ed8effa
jjaeminn <104702271+jjaeminn@users.noreply.github.com>
2022-07-14 15:55:58 +0900

    Add files via upload

--- java 문제 풀이.js
+++ java 문제 풀이.js
@@ -72,3 +72,22 @@ var result = year.concat("/") + month.concat("/") + day.concat("/") + hour.conca
 console.log(result);
 
 
+10.
+
+//크리스마스 트리처럼 별을 찍는 것이므로 일단 별과 공백을 구분으로 하면 별은 1,3,5,7 홀수개씩으로 늘어나고 공백은 5개에서 1개씩 줄어든다. 
+
+
+var star = '';
+for ( var i = 0; i < 5; i++) { // 5번 반복되도록 
+   for (var j = 4; j > i; j--) {
+      star += ' '; // 공백의 수가 점점 감소하는 것을 표현 
+   }
+
+   for (var k = 0; k <= (i * 2); k++ ) { // 처음 둘의 값은 0 이므로 별이 1개가 찍힘 그 다음 항상 2의 배수로 찍히도록 해주는 코드이다. 
+   star += '*';
+   }
+  star += '\n'; // 반복 될때마다 줄바꿈 
+}
+
+
+console.log(star);
