1c9fb1d7d84a016032364c09723ddb0177d82b5d
jjaeminn <104702271+jjaeminn@users.noreply.github.com>
2022-08-10 13:43:18 +0900

    Add files via upload

--- condition.c
+++ condition.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-int main(void)
+int mainlogg(void)
 {
-	int age = 15;
+	/* int age = 15;
 	if (age >= 20)
 		printf("성인입니다.");					//***********조건문에는 ;이 들가면 안됨 --> 구분해주는 것 이니까
 	else
-		printf("미성년자입니다.");
-	return 0;
+		printf("미성년자입니다.");*/
+	int age = 8;
+	if (age >= 8 && age <= 13)    // && = and
+
+
+		return 0;
 }
